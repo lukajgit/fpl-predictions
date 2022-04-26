d3a35d619ab6060881216563056117df7f05f5da
lukajgit <lukajuranovic1@gmail.com>
2022-04-26 11:40:48 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fpl-predictions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fpl_predictions/core_test.clj
@@ -0,0 +1,7 @@
+(ns fpl-predictions.core-test
+  (:require [clojure.test :refer :all]
+            [fpl-predictions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
